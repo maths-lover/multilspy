2fdcccf6748ce369b9ff1aa1ecf7e59dfe5cd6f6
Suraj Pal Singh <dev.suraj.pal@gmail.com>
2024-10-18 10:57:21 +0530

    Add: pyright language server for python instead of jedi

--- src/multilspy/language_server.py
+++ src/multilspy/language_server.py
@@ -13,20 +13,18 @@ import os
 import pathlib
 import threading
 from contextlib import asynccontextmanager, contextmanager
-from .lsp_protocol_handler.lsp_constants import LSPConstants
-from  .lsp_protocol_handler import lsp_types as LSPTypes
+from pathlib import PurePath
+from typing import AsyncIterator, Dict, Iterator, List, Tuple, Union
 
 from . import multilspy_types
-from .multilspy_logger import MultilspyLogger
-from .lsp_protocol_handler.server import (
-    LanguageServerHandler,
-    ProcessLaunchInfo,
-)
-from .multilspy_config import MultilspyConfig, Language
+from .lsp_protocol_handler import lsp_types as LSPTypes
+from .lsp_protocol_handler.lsp_constants import LSPConstants
+from .lsp_protocol_handler.server import (LanguageServerHandler,
+                                          ProcessLaunchInfo)
+from .multilspy_config import Language, MultilspyConfig
 from .multilspy_exceptions import MultilspyException
-from .multilspy_utils import PathUtils, FileUtils, TextUtils
-from pathlib import PurePath
-from typing import AsyncIterator, Iterator, List, Dict, Union, Tuple
+from .multilspy_logger import MultilspyLogger
+from .multilspy_utils import FileUtils, PathUtils, TextUtils
 from .type_helpers import ensure_all_methods_implemented
 
 
@@ -72,25 +70,23 @@ class LanguageServer:
         :return LanguageServer: A language specific LanguageServer instance.
         """
         if config.code_language == Language.PYTHON:
-            from multilspy.language_servers.jedi_language_server.jedi_server import (
-                JediServer,
-            )
+            from multilspy.language_servers.pyright_language_server.pyright_server import \
+                PyrightServer
 
-            return JediServer(config, logger, repository_root_path)
+            return PyrightServer(config, logger, repository_root_path)
         elif config.code_language == Language.JAVA:
-            from multilspy.language_servers.eclipse_jdtls.eclipse_jdtls import (
-                EclipseJDTLS,
-            )
+            from multilspy.language_servers.eclipse_jdtls.eclipse_jdtls import \
+                EclipseJDTLS
 
             return EclipseJDTLS(config, logger, repository_root_path)
         elif config.code_language == Language.RUST:
-            from multilspy.language_servers.rust_analyzer.rust_analyzer import (
-                RustAnalyzer,
-            )
+            from multilspy.language_servers.rust_analyzer.rust_analyzer import \
+                RustAnalyzer
 
             return RustAnalyzer(config, logger, repository_root_path)
         elif config.code_language == Language.CSHARP:
-            from multilspy.language_servers.omnisharp.omnisharp import OmniSharp
+            from multilspy.language_servers.omnisharp.omnisharp import \
+                OmniSharp
 
             return OmniSharp(config, logger, repository_root_path)
         else:
